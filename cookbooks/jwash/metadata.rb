56de4edf7d7d2db2a7a456eb65ec75735c577521
jasonwash <jasonwash@gmail.com>
2012-06-11 18:12:09 +0000

    added hello world recipe

---
+++ cookbooks/jwash/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Jason M. Wash"
+maintainer_email "jasonwash@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures jwash"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "jwash::default","Prints Hello World! to the logs"
+
---
+++ cookbooks/jwash/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: jwash
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Hello World"
